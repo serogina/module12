29f43dd9934da090114fb06010cf571c95704804
serogina <seroginadev@gmail.com>
2024-07-12 12:42:43 +0300

    correction of task2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,64 +1,67 @@
 import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 public class Task2 {
     private static final int n = 20;
-    private static List<String> numbers = new ArrayList<>(n);
+
 
     public static void main(String[] args) {
-        setStringNumbers();
-        System.out.println(String.join(", ", numbers) + ".");
-    }
+        ProcessThread threadFizzBuzz = new ProcessThread(Task2::fizzbuzz);
+        ProcessThread threadFizz = new ProcessThread(Task2::fizz);
+        ProcessThread threadBuzz = new ProcessThread(Task2::buzz);
+        ProcessThread threadNumber = new ProcessThread(Task2::number);
 
-    private static void setStringNumbers(){
-        for (int i = 0; i < n; i++) {
-            numbers.add(""+(i+1));
+        List<ProcessThread> threads = new ArrayList<>();
+        threads.add(threadFizzBuzz);
+        threads.add(threadBuzz);
+        threads.add(threadFizz);
+        threads.add(threadNumber);
+
+        for (ProcessThread thread : threads) {
+            thread.start();
         }
-        ExecutorService executorService = Executors.newFixedThreadPool(4);
-            executorService.execute(() -> fizz());
-            executorService.execute(() -> buzz());
-            executorService.execute(() -> fizzbuzz());
-            executorService.execute(() -> number());
 
-        executorService.shutdown();
-    }
-    private static void fizz(){
         for (int i = 1; i <= n; i++) {
+            for (ProcessThread thread : threads) {
+                thread.process(i);
+            }
+            while (true) {
+                int processedCount = 0;
+                for (ProcessThread thread : threads) {
+                    if (thread.isProcessed()) {
+                        processedCount++;
+                    }
+                }
+                if (processedCount == threads.size()) {
+                    break;
+                }
+            }
+        }
+
+        for (ProcessThread thread : threads) {
+            thread.terminate();
+        }
+    }
+
+    private static void fizz(int i){
             if (i % 3 == 0 && i % 5 != 0) {
-                synchronized (numbers) {
-                    numbers.set(i-1, "fizz");
-                }
+                System.out.print ("fizz"+(i == n ? ".":", "));
             }
-        }
     }
-    private static void buzz(){
-        for (int i = 1; i <= n; i++) {
+    private static void buzz(int i){
             if (i % 5 == 0 && i % 3 != 0) {
-                synchronized (numbers) {
-                    numbers.set(i-1, "buzz");
-                }
+                System.out.print ("buzz"+(i == n ? ".":", "));
             }
-        }
     }
-    private static void fizzbuzz(){
-        for (int i = 1; i <= n; i++) {
+    private static void fizzbuzz(int i){
             if (i % 5 == 0 && i % 3 == 0) {
-                synchronized (numbers) {
-                    numbers.set(i-1, "fizzbuzz");
-                }
+                System.out.print ("fizzbuzz"+(i == n ? ".":", "));
             }
-        }
     }
-    private static void number(){
-        for (int i = 1; i <= n; i++) {
+    private static void number(int i){
             if (i % 5 != 0 && i % 3 != 0) {
-                synchronized (numbers) {
-                    numbers.set(i-1, ""+(i-1));
-                }
+                System.out.print (i+(i == n ? ".":", "));
             }
-        }
     }
 
 }
