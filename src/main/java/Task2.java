781afb2fe6a67af67edbf904f5e9601ca059308b
serogina <seroginadev@gmail.com>
2024-07-14 10:52:17 +0300

    correction of task2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,8 +1,11 @@
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class Task2 {
     private static final int n = 20;
+    private static final Queue<String> outputQueue = new LinkedList<>();
 
 
     public static void main(String[] args) {
@@ -41,27 +44,38 @@ public class Task2 {
         for (ProcessThread thread : threads) {
             thread.terminate();
         }
+
+        while (!outputQueue.isEmpty()) {
+            synchronized (outputQueue) {
+                System.out.print(outputQueue.poll() + (outputQueue.isEmpty() ? "." : ", "));
+            }
+        }
     }
 
     private static void fizz(int i){
             if (i % 3 == 0 && i % 5 != 0) {
-                System.out.print ("fizz"+(i == n ? ".":", "));
+                addToQueue("fizz");
             }
     }
     private static void buzz(int i){
             if (i % 5 == 0 && i % 3 != 0) {
-                System.out.print ("buzz"+(i == n ? ".":", "));
+                addToQueue("buzz");
             }
     }
     private static void fizzbuzz(int i){
             if (i % 5 == 0 && i % 3 == 0) {
-                System.out.print ("fizzbuzz"+(i == n ? ".":", "));
+                addToQueue("fizzbuzz");
             }
     }
     private static void number(int i){
             if (i % 5 != 0 && i % 3 != 0) {
-                System.out.print (i+(i == n ? ".":", "));
+                addToQueue(String.valueOf(i));
             }
+    }
+    private static void addToQueue(String output) {
+        synchronized (outputQueue) {
+            outputQueue.add(output);
+        }
     }
 
 }
