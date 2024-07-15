a6a8d9010bae06d4c0c9b68421c260ab670db72d
serogina <seroginadev@gmail.com>
2024-07-15 16:06:18 +0300

    correction of task2

--- src/main/java/Task2.java
+++ src/main/java/Task2.java
@@ -1,81 +1,118 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class Task2 {
-    private static final int n = 20;
+    private static final int n = 20000;
+    private static int i = 1;
+    private static final Object lock = new Object();
     private static final Queue<String> outputQueue = new LinkedList<>();
 
 
     public static void main(String[] args) {
-        ProcessThread threadFizzBuzz = new ProcessThread(Task2::fizzbuzz);
-        ProcessThread threadFizz = new ProcessThread(Task2::fizz);
-        ProcessThread threadBuzz = new ProcessThread(Task2::buzz);
-        ProcessThread threadNumber = new ProcessThread(Task2::number);
+        Thread threadFizzBuzz = new Thread(Task2::fizzbuzz);
+        Thread threadFizz = new Thread(Task2::fizz);
+        Thread threadBuzz = new Thread(Task2::buzz);
+        Thread threadNumber = new Thread(Task2::number);
 
-        List<ProcessThread> threads = new ArrayList<>();
+        List<Thread> threads = new ArrayList<>();
         threads.add(threadFizzBuzz);
         threads.add(threadBuzz);
         threads.add(threadFizz);
         threads.add(threadNumber);
 
-        for (ProcessThread thread : threads) {
+        for (Thread thread : threads) {
             thread.start();
         }
 
-        for (int i = 1; i <= n; i++) {
-            for (ProcessThread thread : threads) {
-                thread.process(i);
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
             }
-            while (true) {
-                int processedCount = 0;
-                for (ProcessThread thread : threads) {
-                    if (thread.isProcessed()) {
-                        processedCount++;
+        }
+    }
+
+    private static void fizz() {
+        while (true) {
+            synchronized (lock) {
+                while (i <= n && (i % 3 != 0 || i % 5 == 0)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
                     }
                 }
-                if (processedCount == threads.size()) {
-                    break;
+                if (i > n) break;
+                addToQueue("fizz");
+                i++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    private static void buzz() {
+        while (true) {
+            synchronized (lock) {
+                while (i <= n && (i % 5 != 0 || i % 3 == 0)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                if (i > n) break;
+                addToQueue("buzz");
+                i++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    private static void fizzbuzz() {
+        while (true) {
+            synchronized (lock) {
+                while (i <= n && (i % 15 != 0)) {
+                    try {
+                        lock.wait();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                if (i > n) break;
+                addToQueue("fizzbuzz");
+                i++;
+                lock.notifyAll();
+            }
+        }
+    }
+
+    private static void number() {
+            synchronized (lock) {
+                while (i <= n){
+                    while (i <= n && outputQueue.isEmpty() && (i % 3 ==0 || i % 5 == 0)) {
+                        try {
+                            lock.wait();
+                        } catch (InterruptedException e) {
+                            throw new RuntimeException(e);
+                        }
+                    }
+                    if (i > n && outputQueue.isEmpty()) break;
+
+                    String value = outputQueue.poll();
+                        if (value == null) {
+                            System.out.print(i + (i >= n ? "." : ", "));
+                            i++;
+                        } else {
+                            System.out.print(value + (i >= n ? "." : ", "));
+                        }
+                        lock.notifyAll();
                 }
             }
-        }
-
-        for (ProcessThread thread : threads) {
-            thread.terminate();
-        }
-
-        while (!outputQueue.isEmpty()) {
-            synchronized (outputQueue) {
-                System.out.print(outputQueue.poll() + (outputQueue.isEmpty() ? "." : ", "));
-            }
-        }
     }
 
-    private static void fizz(int i){
-            if (i % 3 == 0 && i % 5 != 0) {
-                addToQueue("fizz");
-            }
-    }
-    private static void buzz(int i){
-            if (i % 5 == 0 && i % 3 != 0) {
-                addToQueue("buzz");
-            }
-    }
-    private static void fizzbuzz(int i){
-            if (i % 5 == 0 && i % 3 == 0) {
-                addToQueue("fizzbuzz");
-            }
-    }
-    private static void number(int i){
-            if (i % 5 != 0 && i % 3 != 0) {
-                addToQueue(String.valueOf(i));
-            }
-    }
     private static void addToQueue(String output) {
         synchronized (outputQueue) {
             outputQueue.add(output);
         }
     }
-
 }
